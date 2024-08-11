2a74e08886598e24f7e61bdceec6b572d6fd33f4
Anish-Karthik <anishkarthik.54321@gmail.com>
2024-08-11 16:27:20 +0530

    made the question numbers visible

--- sat-parser-v2.py
+++ sat-parser-v2.py
@@ -443,7 +443,7 @@ for sample_paper, (pdf_path, answer_pdf_path) in enumerate(list(files), start=1)
                 #     print("Error: Question number mismatch")
                 question.correct_option = all_answers[qno_cnt].answer
                 question.detailed_answer = all_answers[qno_cnt].detailed_solution
-                question.qno = str(j + 1)
+                # question.qno = str(j + 1)
                 qno_cnt += 1
 
     for i, obj in enumerate(all_comprehensions):
