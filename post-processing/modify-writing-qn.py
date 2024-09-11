29c6680206ecaa9023f82fc9b7ba9ed1f358fac2
CR3IX <137896785+CR3IX@users.noreply.github.com>
2024-09-11 18:57:39 +0530

    update writing qns

--- post-processing/modify-writing-qn.py
+++ post-processing/modify-writing-qn.py
@@ -1,5 +1,6 @@
 import json
 import os
+import re
 
 
 def get_context(passage_words, ind, step):
@@ -34,7 +35,7 @@ for input_json_path in sorted(os.listdir("sat/writing-comp-output")):
 
         if ("QS$$" in word and word[-1] not in modify_qns_list):
             modify_qns_list.append(word[-1])
-            qns_reference[int(word[-1])] = get_context(passage_words, ind, -1)
+            qns_reference[int(re.findall(r"(\d+)", word)[0])] = get_context(passage_words, ind, -1)
 
         for modify_qn in modify_qns_list:
             if "$$" in word and word[-1] != modify_qn:
@@ -42,14 +43,18 @@ for input_json_path in sorted(os.listdir("sat/writing-comp-output")):
             qns_reference[int(modify_qn)] += word + " "
 
         if ("QE$$" in word and word[-1] in modify_qns_list):
-            qns_reference[int(word[-1])] += get_context(passage_words, ind, 1)
+            qns_reference[int(re.findall(r"(\d+)", word)[0])] += get_context(passage_words, ind, 1)
             modify_qns_list.remove(word[-1])
 
     for i in qns_reference:
         print(i, end="\n\n\n")
     for ind, question in enumerate(input_json["questions"]):
-        input_json["questions"][ind]["description"] += f".....{qns_reference[ind]}....."
+        if qns_reference[ind] == "":
+            continue
+        if input_json["questions"][ind]["description"] != "":
+            input_json["questions"][ind]["description"] += "\n"
+        input_json["questions"][ind]["description"] += f"(.....{qns_reference[ind].strip()}.....)"
 
     output_json = json.dumps(input_json, indent=4)
-    with open(f"sat/writing-comp-output/{input_json_path}", "w") as f:
+    with open(f"{input_json_path}", "w") as f:
         f.write(output_json)
