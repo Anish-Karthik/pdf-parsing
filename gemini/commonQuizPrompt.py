5bda7c316683dfac18d7cd38130e3c09d7cd0253
MuthuBlacky <muthupandid21cs@psnacet.edu.in>
2024-09-12 20:32:01 +0530

    parsing code added

--- gemini/commonQuizPrompt.py
+++ gemini/commonQuizPrompt.py
@@ -1,37 +1,38 @@
 from gemini_api import *
 import re
-import pandas as pd
 import time
 from google.api_core.exceptions import ResourceExhausted
+import json
 
 def get_quiz_prompt():
-  return f"""
+    return """
     give 5 sample english quiz questions with options and correct option
-    Output: give python dict("question":question_description
-    ,"options": [option1,option2,option3,option4],
-    "correct_option":("A" or "B" or "C" or "D")
-    )
+    Output: give python dict("questions" : [{"question":question_description,
+    "options": [option1,option2,option3,option4],
+    "correct_option":("A" or "B" or "C" or "D")]
+    })
     """
 
 def get_quiz_delayed_prompt(delay=15):
-  try:
-    time.sleep(delay)
-    raw_response = model.generate_content(
-      get_quiz_prompt()
-    )
-    # print(raw_response.text)
-    return filter_response(raw_response.text)
-  except ResourceExhausted as e:
-    print("delay:",delay,e)
-    return get_quiz_delayed_prompt(delay*2)
+    try:
+        time.sleep(delay)
+        raw_response = model.generate_content(
+            get_quiz_prompt()
+        )
+        return filter_response(raw_response.text)
+    except ResourceExhausted as e:
+        print("delay:", delay, e)
+        return get_quiz_delayed_prompt(delay * 2)
 
 def filter_response(text):
-  return re.sub("\*{1,2}","",text)
-
-
-
-
-api_key = os.environ.get("GEMINI_API_KEY")
+    # Regular expression pattern to match the code block
+    pattern = r"```python"
+    # Replace the code block with the replacement string
+    replaced_text = re.sub(pattern, '', text, flags=re.DOTALL)
+    pattern = r"```"
+    replaced_text = re.sub(pattern, '', text, flags=re.DOTALL)
+    return replaced_text
+api_key = "AIzaSyBf8XSKPjDG5R640Y1P-CYNFOlZXfE83ws"
 configure_client(api_key)
 
 model = generativeai.GenerativeModel(
@@ -39,11 +40,16 @@ model = generativeai.GenerativeModel(
 )
 
 raw_response = model.generate_content(
-  get_quiz_prompt()
+    get_quiz_prompt()
 )
-with open("output/gemini/raw_response.txt", "w") as f:
-    f.write(raw_response.text)
-
-
-  
-
+print(raw_response.text)
+filtered_text = filter_response(raw_response.text)
+structured_data = filtered_text.replace("python", "")
+print(structured_data)
+json_data = json.dumps(structured_data, indent=2)
+# Save the JSON data to a file if it's not empty
+if json_data:
+    with open("/Users/muthupandi/Desktop/GitHub/pdf-parsing/gemini/output/gemini/raw_response.json", "w") as f:
+        json.dump(json_data, f, indent=2)
+else:
+    print("No valid JSON data to save.")
