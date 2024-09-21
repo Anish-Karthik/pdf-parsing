ea028d0720d883c3c9c35512340090417456fce3
CR3IX <137896785+CR3IX@users.noreply.github.com>
2024-09-21 23:06:54 +0530

    json

--- gemini/split_json.py
+++ gemini/split_json.py
@@ -1,42 +1,25 @@
 import json
 import os
-math_topics = [
-    "Number Systems",
-    "Simplification and Approximation",
-    "Data Interpretation",
-    "Quadratic Equations",
-    "Profit and Loss",
-    "Simple Interest and Compound Interest",
-    "Time and Work",
-    "Time, Distance, and Speed",
-    "Mensuration",
-    "Average",
-    "Ratio and Proportion",
-    "Partnership",
-    "Mixtures and Alligations",
-    "Permutation and Combination",
-    "Probability",
-    "Data Sufficiency",
-    "Inequalities",
-    "Logarithms",
-    "Surds and Indices",
-    "Coordinate Geometry",
-    "Trigonometry",
-    "Algebra",
-    "Geometry",
-    "Calculus"
+para_jumbles_topics = [
+    "Paragraph coherence: Understanding the overall theme and flow of the paragraph",
+    "Logical sequencing: Identifying the correct order of sentences based on cause-and-effect relationships, chronological order, or thematic progression",
+    "Topic sentences: Identifying the main idea of the paragraph",
+    "Supporting details: Understanding how supporting sentences relate to the topic sentence",
+    "Conjunctions and transition words: Recognizing the role of connectors in establishing relationships between sentences",
+    "Contextual clues: Using hints and clues within the paragraph to determine the correct sequence",
+    "Sentence structure and grammar: Understanding how sentence structure and grammar affect the overall coherence and flow"
 ]
 
-sbi_exam_id = 18
-ibps_exam_id = 10
-title = "Quantitative Ability"
+ibps_exam_id = 9
+sbi_exam_id = 16
+title = "Para Jumbles"
 
-json_path_list = sorted([f for f in os.listdir("gemini/output") if f.endswith(".json")])
+json_path_list = sorted([f for f in os.listdir("gemini/output/para_jumbles") if f.endswith(".json")])
 
 print(json_path_list)
 
 for json_path in json_path_list:
-  with open(f'gemini/output/{json_path}', 'r') as json_file:
+  with open(f'gemini/output/para_jumbles/{json_path}', 'r') as json_file:
     data = json.load(json_file)
 
     sbi_questions = []
@@ -52,16 +35,16 @@ for json_path in json_path_list:
     sbi["questions"] = sbi_questions
     sbi["exam_id"] = sbi_exam_id
     sbi["title"] = title
-    sbi["order"] = math_topics.index(json_path[:-5]) + 1
+    sbi["order"] = para_jumbles_topics.index(json_path[:-5]) + 1 + 14
 
     ibps = data.copy()
     ibps["questions"] = ibps_questions
     ibps["exam_id"] = ibps_exam_id
     ibps["title"] = title
-    ibps["order"] = math_topics.index(json_path[:-5]) + 1
+    ibps["order"] = para_jumbles_topics.index(json_path[:-5]) + 1 + 14
 
-    with open(f'gemini/output/sbi/{json_path}', 'w') as json_file:
+    with open(f'gemini/output/sbi/para_jumbles/{json_path}', 'w') as json_file:
       json.dump(sbi, json_file, indent=4)
 
-    with open(f'gemini/output/ibps/{json_path}', 'w') as json_file:
+    with open(f'gemini/output/ibps/para_jumbles/{json_path}', 'w') as json_file:
       json.dump(ibps, json_file, indent=4)
