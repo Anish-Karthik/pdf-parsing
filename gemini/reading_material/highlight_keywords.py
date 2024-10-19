bfb5e995a3216054178b7cde0cc48615cf82fdcf
CR3IX <137896785+CR3IX@users.noreply.github.com>
2024-10-19 22:15:20 +0530

    html modified

--- gemini/reading_material/highlight_keywords.py
+++ gemini/reading_material/highlight_keywords.py
@@ -4,7 +4,7 @@ import threading
 def get_highlighted_html(page):
     prompt = f"""
     
-    wrap all the important data and keywords in the given html in a *div important="true"*
+    wrap 10 important data and keywords in the given html in a *span class="important"*
     length of each important keyword should be less than 3 words
 
     html:
@@ -17,14 +17,15 @@ def get_highlighted_html(page):
 
     page["html_with_keywords"] = page["html_with_keywords"].replace("```html\n", "")
     page["html_with_keywords"] = page["html_with_keywords"].replace("\n```", "")
+    page["html_with_keywords"] = page["html_with_keywords"].replace("\n", "")
+    page["html_with_keywords"] = page["html_with_keywords"].replace("\\\"", r"\"")
 
     output_json.append(page)
 
 output_json = []
 def highlight_keywords():
-  path = "/Users/pranav/GitHub/pdf-parsing/gemini/Neet/reading_material/51_reading_material.json"
-
-
+  path = "/Users/pranav/GitHub/pdf-parsing/gemini/Neet/reading_material/53_reading_material.json"
+  global output_json
 
   with open(path, "r") as f:
       reading_material = json.load(f)
@@ -40,12 +41,13 @@ def highlight_keywords():
 
       with open(path, "w") as f:
           json.dump(output_json, f, indent=4)
+          output_json = []
+highlight_keywords()
+# path = "/Users/pranav/GitHub/pdf-parsing/gemini/Neet/reading_material/51_reading_material.json"
+# json_obj = read_json_file(path)
 
-path = "/Users/pranav/GitHub/pdf-parsing/gemini/Neet/reading_material/51_reading_material.json"
-json_obj = read_json_file(path)
+# for item in json_obj:
+#   item["keyword_array"] = item["keywords"]
+#   item["keywords"] = ", ".join(item["keyword_array"])
 
-for item in json_obj:
-  item["keyword_array"] = item["keywords"]
-  item["keywords"] = ", ".join(item["keyword_array"])
-
-write_json_file(path, json_obj)
+# write_json_file(path, json_obj)
