3b84179a08ac6b2ffe9615cc2f65f945ce2056bc
barath0811 <barath0811@gmail.com>
2024-10-22 17:57:59 +0530

    Adding one more call for creating content

--- gemini/reading_material/reading_material.py
+++ gemini/reading_material/reading_material.py
@@ -34,19 +34,30 @@ def create_page_content(question, neet_pdf):
     The prepared content should be 100-150 words.
     Get to the point. Make sure the content is clear and concise.
     Skip any introductions that are not relevant to the topic.
-    
+
     make the content engaging and easy to read for better understanding. also make sure the content has the answer for
     question:{question["description"]}
     answer:{get_correct_option(question)}
     without giving the questions explicitly"""
     page_content = model.generate_content([prompt, neet_pdf])
-    print(page_content.text)
 
+    html_content = markdown.markdown(page_content.text)
+
+    prompt = f"""
+        {html_content}
+
+        Modify the content to make it more engaging and interesting to read for the students.
+        Make multiple sections, headers as possible for the student to read it step by step and easy to memorize.
+        Highlight important things, make things obvious.
+    """
+
+    page_content = model.generate_content([prompt, neet_pdf])
+    print(page_content.text)
     question["content_html"] = markdown.markdown(page_content.text)
 
 
 def create_reading_material(json_path, neet_pdf):
-    
+
     quiz = read_json_file(json_path)
 
     call_collection_with_threading(
@@ -60,8 +71,8 @@ def create_reading_material(json_path, neet_pdf):
 
 
 for quiz_id in quiz_id_to_pdf_map:
-    json_path = f"/Users/pranav/GitHub/pdf-parsing/gemini/Neet/{quiz_id}_copy.json"
-    neet_pdf = upload_file_to_gemini(f"/Users/pranav/GitHub/pdf-parsing/gemini/Neet/ncert_books/biology/kebo{quiz_id_to_pdf_map[quiz_id]}.pdf")
+    json_path = f"/home/barath/Documents/sat/code/pdf-parsing/gemini/Neet/{quiz_id}.json"
+    neet_pdf = upload_file_to_gemini(f"/home/barath/Documents/Neet Books/kebo1dd/kebo{quiz_id_to_pdf_map[quiz_id]}.pdf")
     populate_question_keywords(json_path, neet_pdf)
     create_reading_material(json_path, neet_pdf)
     highlight_keywords(json_path)
