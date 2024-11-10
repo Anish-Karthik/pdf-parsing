289e18b4874c0c52f56cb5cbf336f597275a636a
CR3IX <137896785+CR3IX@users.noreply.github.com>
2024-11-10 11:00:28 +0530

    material content generation using search

--- gemini/reading_material/reading_material.py
+++ gemini/reading_material/reading_material.py
@@ -1,15 +1,14 @@
 import json
 from gemini_utilities import *
 from question_keyowrd import *
+from content_keyword import *
 from highlight_keywords import *
 import markdown
 import threading
 
 
 quiz_id_to_pdf_map = {
-    "35": "101",
-    # "38": "102",
-    # "39": "103",
+    "31": "101",
 }
 
 
@@ -45,85 +44,40 @@ Focus on delivering both the explanation of the {question["keywords"]} and the a
     response = model.generate_content(prompt)
     question["content_html"] = get_html_from_response(response.text)
 
-#     prompt = f"""
-# Using the following content:
-# {prompt.text}
-
-# Format the above reading material in a way that is both appealing and stimulating, using the following format:
-
-# 1. **Subheadings**: Divide the content into sections with relevant subheadings to organize ideas clearly.
-# 2. **Bullet Points**: Use bullet points to present key facts, concepts, or steps in a structured way.
-# 3. **Bold Key Terms**: Highlight important terms or concepts in bold to make them stand out.
-# 4. **Engaging Language(Simple)**: Use language that sparks curiosity and encourages the reader to explore more and very simple
-# 5. **Short Sentences**: Ensure sentences are short, punchy, simple, and to the point, making the content easier to digest and more memorable.
-
-# The final content should:
-# - Be concise (around 100-150 words).
-# - Use the above formatting to make it visually engaging and easy to read.
-# - Include elements that ignite curiosity and leave the reader wanting to explore further.
-# - skip any introductions
-# """
-#     response = model.generate_content(prompt)
-
     html = get_html_from_response(response.text)
     
     return html
 
-def create_page_content(question, neet_pdf):
-    keywords = question["keywords"]
+def create_question_material_from_search(question, ncert_sentence_wise_embeddings):
+    fact = generate_fact_from_question(question)
+    top_ncert_matches = search_pdf_top_sentences(ncert_sentence_wise_embeddings, fact)
+    content = create_reading_material_content(fact, top_ncert_matches[0])
+    question["content"] = content
 
-    # prompt = f"""prepare a content about {keywords} so that i will be able to answer this
-    # The prepared content should be 100-150 words.
-    # question:{question["description"]}
-    # answer:{get_correct_option(question)}"""
-    # page_content = model.generate_content([prompt, neet_pdf])
-    # print(page_content.text)
+    prompt = f""" **convert the response from llm to a html format**:
+    {content}
+    """
+    response = model.generate_content(prompt)
+    print(response.text)
 
-    prompt = f"""
-    Prepare a reading material about the given question including key details and explanations to help students understand the topic thoroughly.
-The content should also directly address the following question:
+    prompt=f"""
+    html: {response.text}
 
-**Question:** {question['description']}
-**Answer:** {get_correct_option(question)}
-
-The content should also be able to answer atleast 5 questions related to the topic.
-
-Ensure the content is concise (100-150 words), engaging, and clear. Avoid unnecessary introductions.
-Focus on delivering both the explanation of the {keywords} and the answer in a way that helps students easily grasp and retain the information."""
-    page_content = model.generate_content([prompt, neet_pdf])
-    html_content = markdown.markdown(page_content.text)
-
-    prompt = f"""
-Using the following content:
-{html_content}
-
-Create a summary that is both appealing and stimulating, using the following format:
-
-1. **Subheadings**: Divide the content into sections with relevant subheadings to organize ideas clearly.
-2. **Bullet Points**: Use bullet points to present key facts, concepts, or steps in a structured way.
-3. **Bold Key Terms**: Highlight important terms or concepts in bold to make them stand out.
-4. **Engaging Language(Simple)**: Use language that sparks curiosity and encourages the reader to explore more and very simple
-5. **Short Sentences**: Ensure sentences are short, punchy, simple, and to the point, making the content easier to digest and more memorable.
-
-The final summary should:
-- Be concise (around 100-150 words).
-- Use the above formatting to make it visually engaging and easy to read.
-- Include elements that ignite curiosity and leave the reader wanting to explore further.
-- skip any introductions
-"""
-    page_content = model.generate_content(prompt)
+    split the html into sections with relevant subheadings to organize ideas clearly.
+    add <hr> between each section
+    """
+    response = model.generate_content(prompt)
+    question["content_html"] = get_html_from_response(response.text)
     
-    print(page_content.text)
-    question["content_html"] = markdown.markdown(page_content.text)
 
 
-def create_reading_material(json_path, neet_pdf):
+def create_reading_material(json_path, ncert_sentence_wise_embeddings):
 
     quiz = read_json_file(json_path)
 
     call_collection_with_threading(
-        func=create_question_material,
-        args=(),
+        func=create_question_material_from_search,
+        args=(ncert_sentence_wise_embeddings,),
         threads=10,
         collection=quiz["questions"]
     )
@@ -132,10 +86,18 @@ def create_reading_material(json_path, neet_pdf):
 
 
 for quiz_id in quiz_id_to_pdf_map:
-    json_path = f"/Users/pranav/GitHub/pdf-parsing/gemini/Neet/35.json"
-    neet_pdf = upload_file_to_gemini(f"/Users/pranav/GitHub/pdf-parsing/gemini/Neet/ncert_books/biology/kebo{quiz_id_to_pdf_map[quiz_id]}.pdf")
+    json_path = f"/Users/pranav/GitHub/pdf-parsing/gemini/Neet/{quiz_id}.json"
+    neet_pdf_path = f"/Users/pranav/GitHub/pdf-parsing/gemini/Neet/ncert_books/biology/kebo{quiz_id_to_pdf_map[quiz_id]}.pdf"
+    neet_pdf = upload_file_to_gemini(neet_pdf_path)
     populate_question_keywords(json_path, neet_pdf)
-    create_reading_material(json_path, neet_pdf)
+    print("populated question keywords...")
+    
+    ncert_sentence_wise_embeddings: list[SentenceWiseEmbeddings] = get_sentence_wise_embeddings(neet_pdf_path)
+    print("got sentence wise embeddings...")
+    
+    create_reading_material(json_path, ncert_sentence_wise_embeddings)
+    print("created reading material...")
+    
     highlight_keywords(json_path)
 
     
