1dcafb3c22b955042f779044cc9c43dbb9f3e765
CR3IX <137896785+CR3IX@users.noreply.github.com>
2024-11-21 14:24:30 +0530

    updated minor fixes

--- gemini/reading_material/reading_material_tamil.py
+++ gemini/reading_material/reading_material_tamil.py
@@ -31,7 +31,7 @@ def get_all_pdf_paths_for_topic(topic) -> list[str]:
     for csv_path in sorted(csv_paths):
         start_end_list = find_all_pages_for_topic(os.path.join(csv_paths_dir, csv_path), topic)
         for start_end in start_end_list:
-            pdf_path = f"/Users/pranav/GitHub/pdf-parsing/qgen/generated/{csv_path.removesuffix(".csv")}th_Std_Tamil_CBSE-{start_end}.pdf"
+            pdf_path = f"/Users/pranav/GitHub/pdf-parsing/qgen/generated/{csv_path.removesuffix(".csv")}th-{start_end}.pdf"
             pdf_paths.append(pdf_path)
     return pdf_paths
 
@@ -315,13 +315,11 @@ def change_name():
         new_name = f"{group[0]}-{group[1]}-{group[2]}.pdf"
         os.rename(os.path.join(dir_path, pdf_path), os.path.join(dir_path, new_name))
         
-
-change_name()
-# for quiz_id in quiz_id_to_topic_map:
+for quiz_id in quiz_id_to_topic_map:
 #     # print("tamil reading material")
-#     json_path = f"/Users/pranav/GitHub/pdf-parsing/gemini/tamil/{quiz_id}"
-#     pdf_paths = get_all_pdf_paths_for_topic(quiz_id_to_topic_map[quiz_id])
-#     create_material_for_topic(json_path, quiz_id_to_topic_map[quiz_id], pdf_paths)
+    json_path = f"/Users/pranav/GitHub/pdf-parsing/gemini/tamil/{quiz_id}"
+    pdf_paths = get_all_pdf_paths_for_topic(quiz_id_to_topic_map[quiz_id])
+    create_material_for_topic(json_path, quiz_id_to_topic_map[quiz_id], pdf_paths)
     # tamil_pdf_path = f"/Users/pranav/GitHub/pdf-parsing/qgen/generated/{quiz_id_to_pdf_map[quiz_id]}"
     # populate_question_keywords_tamil(json_path, tamil_pdf_path)
     # # print("populated tamil question keywords")
